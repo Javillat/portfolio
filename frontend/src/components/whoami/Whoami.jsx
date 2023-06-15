fa7f1bf6a642376bbaee0c06f4d4807a6e7284d9
Javier Villatoro <wemakesoftx@gmail.com>
2023-06-15 11:57:36 -0600

    chore: Agregar el menu

--- frontend/src/components/whoami/Whoami.jsx
+++ frontend/src/components/whoami/Whoami.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Menu from "../menu/Menu";
+
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
@@ -15,78 +17,81 @@ export default function Whoami() {
   console.log(orangecolor);
 
   return (
-    <div className="whoami_container">
-      <header className="header_whoami">
-        <p>Who Am I:</p>
-      </header>
-      <div className="typewriter">
-        <h1>Francisco Javier Villatoro</h1>
+    <>
+      <Menu />
+      <div className="whoami_container">
+        <header className="header_whoami">
+          <p>Who Am I:</p>
+        </header>
+        <div className="typewriter">
+          <h1>Francisco Javier Villatoro</h1>
+        </div>
+        <div className="fullstack">
+          <h2>-[ Full Stack Developer ]-</h2>
+        </div>
+        <div className="whoami_icons">
+          <section className="linkedin">
+            <a
+              aria-label="LinkedIn Profile"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/in/javillatoro-full-stack"
+            >
+              <LinkedInIcon color="warning" fontSize="large" sx={{ "&:hover": { color: "white" } }} />
+            </a>
+          </section>
+          <section className="github">
+            <a
+              aria-label="Github Profile"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.github.com/Javillat"
+            >
+              <GitHubIcon color="warning" fontSize="large" sx={{ "&:hover": { color: "white" } }} />
+            </a>
+          </section>
+          <section className="whatsapp">
+            <a
+              aria-label="WhatsApp Link"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://wa.me/+50372146998"
+            >
+              <WhatsAppIcon color="warning" fontSize="large" sx={{ "&:hover": { color: "white" } }} />
+            </a>
+          </section>
+          <section className="telegram">
+            <a
+              aria-label="Telegram Link"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://t.me/Javillatoro"
+            >
+              <TelegramIcon color="warning" fontSize="large" sx={{ "&:hover": { color: "white" } }} />
+            </a>
+          </section>
+          <section className="number">
+            <a
+              aria-label="Contact Number"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="tel:+50372146998"
+            >
+              <ContactPhoneIcon color="warning" fontSize="large" sx={{ "&:hover": { color: "white" } }} />
+            </a>
+          </section>
+          <section className="email">
+            <a
+              aria-label="Email Contact"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="mailto:fjvillatoroc@gmail.com"
+            >
+              <EmailIcon color="warning" fontSize="large" sx={{ "&:hover": { color: "white" } }} />
+            </a>
+          </section>
+        </div>
       </div>
-      <div className="fullstack">
-        <h2>-[ Full Stack Developer ]-</h2>
-      </div>
-      <div className="whoami_icons">
-        <section className="linkedin">
-          <a
-            aria-label="LinkedIn Profile"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.linkedin.com/in/javillatoro-full-stack"
-          >
-            <LinkedInIcon color="warning"  fontSize="large" sx={{ "&:hover": { color: "white" } }} />
-          </a>
-        </section>
-        <section className="github">
-          <a
-            aria-label="Github Profile"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.github.com/Javillat"
-          >
-            <GitHubIcon color="warning" fontSize="large" sx={{ "&:hover": { color: "white" } }} />
-          </a>
-        </section>
-        <section className="whatsapp">
-          <a
-            aria-label="WhatsApp Link"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://wa.me/+50372146998"
-          >
-            <WhatsAppIcon color="warning" fontSize="large" sx={{ "&:hover": { color: "white" } }} />
-          </a>
-        </section>
-        <section className="telegram">
-          <a
-            aria-label="Telegram Link"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://t.me/Javillatoro"
-          >
-            <TelegramIcon color="warning" fontSize="large" sx={{ "&:hover": { color: "white" } }} />
-          </a>
-        </section>
-        <section className="number">
-          <a
-            aria-label="Contact Number"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="tel:+50372146998"
-          >
-            <ContactPhoneIcon color="warning" fontSize="large" sx={{ "&:hover": { color: "white" } }} />
-          </a>
-        </section>
-        <section className="email">
-          <a
-            aria-label="Email Contact"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="mailto:fjvillatoroc@gmail.com"
-          >
-            <EmailIcon color="warning" fontSize="large" sx={{ "&:hover": { color: "white" } }} />
-          </a>
-        </section>
-      </div>
-    </div>
+    </>
   );
 }
