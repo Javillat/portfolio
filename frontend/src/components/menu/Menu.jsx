2d3d20de199de2e7e3be3eda39738a4a93ced27b
Javier Villatoro <wemakesoftx@gmail.com>
2023-06-15 11:58:31 -0600

    refactor: Reconfigurar menu

--- frontend/src/components/menu/Menu.jsx
+++ frontend/src/components/menu/Menu.jsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom";
 import { _LANGUAGES } from "../config/index";
 import { useState } from "react";
+import { ToggleButton } from "@mui/material";
 
 export default function Menu() {
 
-    const { isActive, setActive } = useState(true)
+    const { isActive, setActive } = useState(false);
+    const [selected, setSelected] = useState(false);
 
     return (
         <nav>
@@ -12,14 +14,17 @@ export default function Menu() {
                 <NavLink className={isActive} to='/'>Home</NavLink>
                 <NavLink className={isActive} to='/About'>About</NavLink>
             </div>
+            <ToggleButton value="check" selected={selected} onChange={() => { setSelected(!selected) }}>
+            </ToggleButton>
 
-            <select defaultValue={'es'}>
+            {/* <select defaultValue={'es'}>
 
                 {_LANGUAGES.map(({ code, label }) => (
                     <option key={code} value={code}>{label}</option>
                 ))
                 };
-            </select>
+            </select> */}
+
         </nav>
     );
 };
