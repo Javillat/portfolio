012398917da16486fa00ccf7ccc5c1d889c0329a
Javier Villatoro <wemakesoftx@gmail.com>
2023-06-15 11:59:27 -0600

    chore: Crear rutas y agregar whoami

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,36 @@
 
 import { useCallback } from "react";
+import { Route, Routes } from "react-router-dom";
 import Particles from "react-particles";
 import { loadFull } from "tsparticles";
 import Whoami from './components/whoami/Whoami';
 import About from './components/about/About';
 import './App.css';
+import Menu from "./components/menu/Menu";
 //https://particles.js.org/
 
 function App() {
   const particlesInit = useCallback(async engine => {
     console.log(engine);
     await loadFull(engine);
-  },[]);
+  }, []);
 
   const particlesLoaded = useCallback(async container => {
     await console.log(container);
 
-  },[]);
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <Particles id="tsparticles" url="http://localhost:3001/json" init={particlesInit} loaded={particlesLoaded} />
-        <Whoami />
+        <Routes>
+          <Route path='/' element={<Whoami />} />
+        </Routes>
       </header>
       <section id="about">
         {/* <About /> */}
       </section>
-        <h1>Soy portfolio</h1> 
+      <h1>Soy portfolio</h1>
     </div>
   );
 }
