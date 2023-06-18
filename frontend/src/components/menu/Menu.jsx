e146521ec6c6882867584d08d1eeb8e9fe56eae2
Javier Villatoro <wemakesoftx@gmail.com>
2023-06-18 13:35:07 -0600

    chore: Agregar menus/estilizar

--- frontend/src/components/menu/Menu.jsx
+++ frontend/src/components/menu/Menu.jsx
@@ -1,15 +1,15 @@
 import { NavLink } from "react-router-dom";
 import { _LANGUAGES } from "../config/index";
 import { useState } from "react";
+
 import Switch from "@mui/material/Switch";
+import './menu.css';
 
-const label = {inputProps: {'ES':'EN'}}
 
-const isActive = ({ isActive }) => `link ${isActive ? 'active' : ''}`;
+const isActive = ({ isActive }) => `${isActive ? 'active' : ''}`;
 export default function Menu() {
 
-    //const { isActive, setActive } = useState(false);
-    const [selected, setSelected] = useState(false);
+    //const [selected, setSelected] = useState(false);
     const [checked, setChecked] = useState(true);
 
     const handleChange = (event) => {
@@ -20,14 +20,21 @@ export default function Menu() {
         <nav>
             <div>
                 <NavLink className={isActive} to='/'>Home</NavLink>
-                <NavLink className={isActive} to='/'>Resume</NavLink>
-                <NavLink className={isActive} to='/About'>About</NavLink>
+                <NavLink className={isActive} to='/resume'>Resume</NavLink>
+                <NavLink className={isActive} to='/projects'>Projects</NavLink>
+                <NavLink className={isActive} to='/tech'>Tech</NavLink>
+                <NavLink className={isActive} to='/achievment'>Achievment</NavLink>
+                <NavLink className={isActive} to='/certificate'>Certificate</NavLink>
+                <NavLink className={isActive} to='/contact'>Contact</NavLink>
+                <NavLink className={isActive} to='/recommended'>Recommend</NavLink>
+                <NavLink className={isActive} to='/about'>About</NavLink>
             </div>
-            {
+            {/* {
                  _LANGUAGES.map((lng) => {
                     return lng.code
                 })
-            }
+            } */}
+            {/*<button onClick={()=>{setSelected(!selected)}}>{selected?'Hide':'Show'}Menu<*/}
             <Switch checked={checked} onChange={handleChange} inputProps={{'aria-label':_LANGUAGES.map((lng) => {
                     return lng.code
                 })}} size="medium" />
