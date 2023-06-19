56b505cd61c979f6e1b359174319bc90ee1eec78
Javier Villatoro <wemakesoftx@gmail.com>
2023-06-19 01:10:06 -0600

    chore: Agregar rutas de idiomas

--- backend/src/route/index.js
+++ backend/src/route/index.js
@@ -1,7 +1,11 @@
 const { Router } = require("express");
 const routejs = require("./routejson.js");
+const routeres = require("./routeres.js")
+const routeren = require("./routeren.js")
 const routerMain = Router();
 
-routerMain.use("/json", routejs)
+routerMain.use("/json", routejs);
+routerMain.use("/en.json", routeren);
+routerMain.use("/es.json", routeres);
 
 module.exports = routerMain;
