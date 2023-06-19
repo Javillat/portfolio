b730d6b78fda622bc6344f3302cc4ce7ce44ce35
Javier Villatoro <wemakesoftx@gmail.com>
2023-06-18 23:23:22 -0600

    chore: Actualizar ruta de recurso

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,7 +1,11 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import i18nBackend from 'i18next-http-backend';
+
+//const getCurrentHost = import.meta.env.MODE = 'development' ? 'http://localhost:5173' : 'LINK TO PROD';
 
 i18n
+    .use(i18nBackend) // passes i18nBackend down to react
     .use(initReactI18next) // passes i18n down to react
     .init({
         lng: 'es',
@@ -9,10 +13,14 @@ i18n
         interpolation: {
             escapeValue: false
         },
-        resources: {
-            es: require('../src/components/languages/es.json'),
-            en: require('../src/components/languages/en.json')
+        backend: {
+            //loadPath: `${getCurrentHost}/i18n/{{lng}}.json`,
+            loadPath: 'http://localhost:5173/i18n/languages/{{lng}}.json'
         }
+        // resources: {
+        //     es: require('../src/components/languages/es.json'),
+        //     en: require('../src/components/languages/en.json')
+        // }
     });
 
 
