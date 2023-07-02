8fdcd4666aac101bfc44de1ea3a53b37fd4e8be4
Javier Villatoro <wemakesoftx@gmail.com>
2023-07-02 12:58:35 -0600

    feat: Agregar el modulo de Projects

---
+++ frontend/src/components/projects/Projects.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+
+const Projects = () => {
+    //const [projectsData] = useState([]);
+
+    return(
+        <div>
+            This is the projects page. Coming soon...!
+        </div>
+    );
+}
+
+export default Projects;
