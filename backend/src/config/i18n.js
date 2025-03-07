90ec7bcc3ecbea139222090776ddb3d83504e804
Javier Villatoro <wemakesoftx@gmail.com>
2025-03-07 12:50:22 -0600

    chore: Modificacion de idiomas, texto resumen

---
+++ backend/src/config/i18n.js
@@ -0,0 +1,29 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import i18nBackend from 'i18next-http-backend';
+
+//const getCurrentHost = import.meta.env.MODE = 'development' ? 'http://localhost:5173' : 'LINK TO PROD';
+
+i18n
+    .use(i18nBackend) // passes i18nBackend down to react
+    .use(initReactI18next) // passes i18n down to react
+    .init({
+        lng: 'es',
+        //lng: getCurrentLang(),
+        fallbackLng: 'es',
+        interpolation: {
+            escapeValue: false
+        },
+        backend: {
+            //loadPath: `${getCurrentHost}/i18n/{{lng}}.json`,
+            loadPath: 'http://localhost:3001/{{lng}}.json'
+            // loadPath: '../public/locales/{{lng}}.json'
+        }
+        // resources: {
+        //     es: require('../src/components/languages/es.json'),
+        //     en: require('../src/components/languages/en.json')
+        // }
+    });
+
+
+export default i18n;
