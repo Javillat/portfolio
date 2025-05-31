6917dcc7d8307c8437c9a46aeea846a6a94aad14
Javier Villatoro <wemakesoftx@gmail.com>
2025-05-30 18:05:32 -0600

    docs: Comentar codigo

--- frontend/src/blog/hooks/useFetch.js
+++ frontend/src/blog/hooks/useFetch.js
@@ -2,10 +2,8 @@
 export const useFetch = async (url) => {
     try {
         const response = await fetch(url);
-        console.log(`Fetching data from ${url}`);
         console.log(`Response status: ${response.status}`);
-        
-        
+
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
